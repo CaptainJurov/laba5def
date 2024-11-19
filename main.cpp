9c924702804b507216afb7cff642531c55a7efca
CaptainJurov <goudazv@gmail.com>
2024-11-20 06:52:03 +0700

    Скелет

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "colors.h"
+#include <iostream>
+#include <istream>
+struct Звено {
+    int a;
+    float k;
+    Звено* next;
+};
+struct math {
+private:
+    Звено* create(int n) {
+    }
+public:
+    Звено first;
+    math(std::iostream& is) {
+
+    }
+};
